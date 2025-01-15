bb8dfcb1f24da6d8a608f186d58da19292fe750a
AkashSDas <aakashdas368@gmail.com>
2025-01-15 23:38:32 +0530

    [fe] fix jsx in HowItWorksExample

--- frontend/src/components/hero/HowItWorksExample.tsx
+++ frontend/src/components/hero/HowItWorksExample.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRightIcon, PlusIcon } from "lucide-react";
+import { ArrowUpRightIcon, EqualIcon, PlusIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,10 +15,10 @@ export function HowItWorksExample() {
                 <ExampleImage imgURL="/images/input-img1.png" label="Images/Video" />
 
                 <div className="border z-20 border-neutral-600/40 font-head bg-neutral-700/90 backdrop:blur-md absolute bottom-1/2 right-1/3 translate-x-1/2 translate-y-1/2 px-4 h-9 flex justify-center items-center rounded-full">
-                    <PlusIcon />
+                    <EqualIcon />
                 </div>
 
-                <ExampleImage imgURL="/images/output-img1.png" label="Filter" />
+                <ExampleImage imgURL="/images/filter-img1.png" label="Filter" />
 
                 <div className="border z-20 border-neutral-600/40 font-head bg-neutral-700/90 backdrop:blur-md absolute bottom-1/2 left-1/3 -translate-x-1/2 translate-y-1/2 px-4 h-9 flex justify-center items-center rounded-full">
                     <PlusIcon />
